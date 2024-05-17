d45e482af5df2f656778a458d2a78762c72f4a12
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-17 15:18:35 +0530

    style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import AuthContext from './context/AuthProvider';
-import Register from './components/pages/SignUp';
-import Home from './components/pages/Home'; 
+//import Register from './components/pages/Register';
 import Login from './components/pages/Login';
+import Home from './components/pages/Home'; 
+//import Adduser from './components/pages/Adduser';
+//import AddAdmin from './components/pages/AddAdmin';
+//import UpdateUser from './components/pages/UpdateUser';
+//import UpdateAdmin from './components/pages/UpdateAdmin';
 
 function Default() {
   const navigate = useNavigate();
   useEffect(() => {
-    navigate('/Navbar');
+    navigate('/home');
   }, [navigate]);
 
   return null;
@@ -19,8 +23,16 @@ function App() {
     <Router>
       <main className="App">
         <Routes>
-          <Route path="./componrnts/pages/login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/" element={<Default />} />
+          <Route path="/Adduser" element={<Adduser/>} />
+          <Route path="/AddAdmin" element={<AddAdmin/>} />
+          <Route path="/UpdateUser" element={<UpdateUser/>} />
+          <Route path="/UpdateAdmin" element={<UpdateAdmin/>} />
+
+
         </Routes>
       </main>
     </Router>
