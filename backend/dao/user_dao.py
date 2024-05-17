9f71cfcbcdd90dc135a324bfa1f7a450427d27f5
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-17 10:51:25 +0530

    dao broken into mul
    Co-Authored-By: Nadee Tharuka <121155318+nadeetharuka@users.noreply.github.com>

--- backend/dao/user_dao.py
+++ backend/dao/user_dao.py
@@ -1,55 +1,29 @@
 from datetime import datetime
 
 import mysql.connector
-from mysql.connector import errorcode
 
+from dao.database_connection import DatabaseConnection
 from models.user_model import User, UserInDB
-
-"""
-    middleware for accessing the user database and performing CRUD operations on the user table
-"""
 
 
 class UserDAO:
-    def __init__(self, host, user, password, database):
-        self.host = host
-        self.user = user
-        self.password = password
-        self.database = database
-        self.cnx = None
-
-    def connect(self):
-        try:
-            self.cnx = mysql.connector.connect(
-                host=self.host,
-                user=self.user,
-                password=self.password,
-                database=self.database
-            )
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
-
-    def disconnect(self):
-        if self.cnx is not None:
-            self.cnx.close()
+    def __init__(self, database_connection: DatabaseConnection):
+        self.db_connection = database_connection
 
     def create_user(self, user: User):
-        cursor = self.cnx.cursor()
+        cnx = self.db_connection.get_connection()
+        cursor = cnx.cursor()
         add_user = ("INSERT INTO users "
                     "(id, username, email, is_admin, hashed_password, mobile, fist_name, last_name, dtp_token) "
                     "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
         data_user = (user.id, user.username, user.email, user.is_admin, user.hashed_password)
         cursor.execute(add_user, data_user)
-        self.cnx.commit()
+        cnx.commit()
         cursor.close()
 
     def get_user_by_username(self, username: str) -> UserInDB | None:
-        cursor = self.cnx.cursor()
+        cnx = self.db_connection.get_connection()
+        cursor = cnx.cursor()
         query = ("SELECT id, username, email, is_admin, hashed_password, mobile, fist_name, last_name, dtp_token "
                  "FROM users "
                  "WHERE username = %s")
@@ -58,10 +32,13 @@ class UserDAO:
         cursor.close()
         if row is None:
             return None
-        return UserInDB(**dict(zip(['id', 'username', 'email', 'is_admin', 'hashed_password', 'mobile', 'fist_name', 'last_name', 'dtp_token'], row)))
+        return UserInDB(**dict(
+            zip(['id', 'username', 'email', 'is_admin', 'hashed_password', 'mobile', 'fist_name', 'last_name',
+                 'dtp_token'], row)))
 
     def get_last_user_id(self) -> int:
-        cursor = self.cnx.cursor()
+        cnx = self.db_connection.get_connection()
+        cursor = cnx.cursor()
         query = "SELECT MAX(id) FROM users"
         cursor.execute(query)
         row = cursor.fetchone()
@@ -75,12 +52,13 @@ class UserDAO:
         Add a token to the blacklist table with the current timestamp
         """
         try:
-            cursor = self.cnx.cursor()
+            cnx = self.db_connection.get_connection()
+            cursor = cnx.cursor()
             query = "INSERT INTO blacklist (token, blacklisted_on) VALUES (%s, %s)"
             timestamp = datetime.now()
             values = (token, timestamp)
             cursor.execute(query, values)
-            self.cnx.commit()
+            cnx.commit()
             cursor.close()
         except mysql.connector.Error as err:
             print(err)
@@ -90,7 +68,8 @@ class UserDAO:
         Check if a token exists in the blacklist table
         """
         try:
-            cursor = self.cnx.cursor()
+            cnx = self.db_connection.get_connection()
+            cursor = cnx.cursor()
             query = "SELECT COUNT(*) FROM blacklist WHERE token = %s"
             values = (token,)
             cursor.execute(query, values)
