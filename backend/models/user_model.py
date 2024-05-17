ac90116f7733b017e138ecc5c39162408f6c5af0
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-17 10:16:39 +0530

    Update user_model.py

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -6,6 +6,9 @@ class User(BaseModel):
     username: str | None = None
     email: str | None = None
     is_admin: bool | None = None
+    mobile: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
 
 
 class UserInDB(User):
