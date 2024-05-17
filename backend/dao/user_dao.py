b0b8d9ba30db5d7555835053efdbc0c8961c0f85
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-17 10:16:55 +0530

    back to mysql

--- backend/dao/user_dao.py
+++ backend/dao/user_dao.py
@@ -1,7 +1,13 @@
 from datetime import datetime
 
-from pymongo import MongoClient
+import mysql.connector
+from mysql.connector import errorcode
+
 from models.user_model import User, UserInDB
+
+"""
+    middleware for accessing the user database and performing CRUD operations on the user table
+"""
 
 
 class UserDAO:
@@ -10,41 +16,87 @@ class UserDAO:
         self.user = user
         self.password = password
         self.database = database
-        self.client = None
-        self.db = None
+        self.cnx = None
 
     def connect(self):
-        self.client = MongoClient(self.host, username=self.user, password=self.password)
-        self.db = self.client[self.database]
+        try:
+            self.cnx = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
 
     def disconnect(self):
-        if self.client is not None:
-            self.client.close()
+        if self.cnx is not None:
+            self.cnx.close()
 
     def create_user(self, user: User):
-        users = self.db.users
-        user_dict = user.dict()
-        users.insert_one(user_dict)
+        cursor = self.cnx.cursor()
+        add_user = ("INSERT INTO users "
+                    "(id, username, email, is_admin, hashed_password) "
+                    "VALUES (%s, %s, %s, %s, %s)")
+        data_user = (user.id, user.username, user.email, user.is_admin, user.hashed_password)
+        cursor.execute(add_user, data_user)
+        self.cnx.commit()
+        cursor.close()
 
     def get_user_by_username(self, username: str) -> UserInDB | None:
-        users = self.db.users
-        user_data = users.find_one({"username": username})
-        if user_data is None:
+        cursor = self.cnx.cursor()
+        query = ("SELECT id, username, email, is_admin, hashed_password "
+                 "FROM users "
+                 "WHERE username = %s")
+        cursor.execute(query, (username,))
+        row = cursor.fetchone()
+        cursor.close()
+        if row is None:
             return None
-        return UserInDB(**user_data)
+        return UserInDB(**dict(zip(['id', 'username', 'email', 'is_admin', 'hashed_password'], row)))
 
     def get_last_user_id(self) -> int:
-        users = self.db.users
-        last_user = users.find().sort("id", -1).limit(1)
-        if last_user is None:
+        cursor = self.cnx.cursor()
+        query = "SELECT MAX(id) FROM users"
+        cursor.execute(query)
+        row = cursor.fetchone()
+        cursor.close()
+        if row is None:
             return 0
-        return last_user[0]["id"]
+        return row[0]
 
     def blacklist_token(self, token: str):
-        blacklist = self.db.blacklist
-        blacklist.insert_one({"token": token, "blacklisted_on": datetime.now()})
+        """
+        Add a token to the blacklist table with the current timestamp
+        """
+        try:
+            cursor = self.cnx.cursor()
+            query = "INSERT INTO blacklist (token, blacklisted_on) VALUES (%s, %s)"
+            timestamp = datetime.now()
+            values = (token, timestamp)
+            cursor.execute(query, values)
+            self.cnx.commit()
+            cursor.close()
+        except mysql.connector.Error as err:
+            print(err)
 
     def is_token_blacklisted(self, token: str) -> bool:
-        blacklist = self.db.blacklist
-        blacklisted_token = blacklist.find_one({"token": token})
-        return blacklisted_token is not None
+        """
+        Check if a token exists in the blacklist table
+        """
+        try:
+            cursor = self.cnx.cursor()
+            query = "SELECT COUNT(*) FROM blacklist WHERE token = %s"
+            values = (token,)
+            cursor.execute(query, values)
+            result = cursor.fetchone()[0]
+            cursor.close()
+            return result > 0
+        except mysql.connector.Error as err:
+            print(err)
+            return False
