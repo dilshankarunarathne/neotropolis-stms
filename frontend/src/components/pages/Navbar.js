2625042c41d3638af354861e75583df4f22c9067
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-05-17 14:33:38 +0530

    css

---
+++ frontend/src/components/pages/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <section>
+        <button>Sign In</button>
+        <button>Sign Up</button>
+    </section>
+
+  )
+}
+ 
