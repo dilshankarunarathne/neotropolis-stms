a80413807b1cc146a268a9b7ca27f2dcac734f1b
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-25 16:41:12 +0530

    routing

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,21 +1,17 @@
 import { useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '../../context/AuthProvider';
+import { Link } from 'react-router-dom';
 
 function Home() {
   const { auth } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!auth.user) {
-      navigate('/login');
-    }
-  }, [auth, navigate]);
-
     return (
         <div>
             <h1>Welcome to Godai!</h1>
             <p>Unleashing AI for Rapid Prototyping and Streamlined Development</p>
+            <Link to='/About'>go to about page</Link>
         </div>
     );
 }
